9c484787c2a5f92c18233bbd399068488e690f3e
dddario-tdf-edu <davidwiz.one@gmail.com>
2026-05-12 19:45:18 -0300

    feat: añadido el modelo Prestamo

--- catalogo/models.py
+++ catalogo/models.py
@@ -65,16 +65,8 @@ class Prestamo(models.Model):
     Si fecha_devolucion es NULL → el préstamo está activo.
     """
 
-    # TODO: implementar los campos:
-    # libro              → ForeignKey(Libro, on_delete=models.CASCADE)
-    # nombre_prestatario → CharField
-    # fecha_prestamo     → DateField
-    # fecha_devolucion   → DateField (null=True, blank=True)
-    #
-    # Preguntas guía:
-    # ¿Por qué usamos CASCADE aquí y PROTECT en Libro→Autor?
-    # ¿Qué valor por defecto tendría sentido para fecha_prestamo?
-    # Tip: podés usar default=timezone.now si querés fecha automática,
-    #      o dejarlo sin default para que el test lo defina explícitamente.
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    nombre_prestatario = models.CharField(max_length=200)
+    fecha_prestamo = models.DateField()
+    fecha_devolucion = models.DateField(null=True, blank=True)
 
-    pass
