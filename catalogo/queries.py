0fb61c65103727a2cda0a034b9de3d55cc794daf
dddario-tdf-edu <davidwiz.one@gmail.com>
2026-05-12 19:48:38 -0300

    feat: añadida la query para traer libros sin disponibilidad

--- catalogo/queries.py
+++ catalogo/queries.py
@@ -18,23 +18,10 @@ def autores_con_mas_de_n_libros(n: int):
 
 
 def libros_sin_disponibilidad():
-    """
-    Devuelve un QuerySet de Libros donde no hay copias disponibles.
-    (prestamos_activos == cantidad_total)
-
-    Returns:
-        QuerySet[Libro]
-
-    Restricción: resolver con ORM, SIN iterar libros en Python
-    (no usar disponibles() en un loop).
-
-    Pista: podés contar los préstamos activos por libro con annotate:
-        Libro.objects.annotate(
-            activos=Count("prestamo", filter=Q(prestamo__fecha_devolucion__isnull=True))
-        ).filter(activos=models.F("cantidad_total"))
-    """
-    # TODO: implementar con annotate + F expression + filter
-    raise NotImplementedError
+    """Devuelve un QuerySet de Libros donde no hay copias disponibles."""
+    return Libro.objects.annotate(
+        activos=Count("prestamo", filter=Q(prestamo__fecha_devolucion__isnull=True))
+    ).filter(activos=F("cantidad_total"))
 
 
 def top_n_libros_mas_prestados(n: int):
